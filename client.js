c72002fc8bc70ca6800c0c6cbc7301eece8b9f3d
Brii SwordLegz <brii.swordlegz@gmail.com>
2024-07-09 21:01:50 -0500

    did not quite finish this up

--- client.js
+++ client.js
@@ -46,12 +46,65 @@ console.log('array of employee data: ',  employees );
 
 
 
+
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
   
-  
+  // No loop.
+  // Check the rating and compare against the bonus percentage.
+let bonusPercent = 0;
+
+if (employee.reviewRating === 3) {
+  bonusPercent += 4;
+}
+if (employee.reviewRating === 4) {
+  bonusPercent += 6;
+}
+if (employee.reviewRating === 5) {
+  bonusPercent += 10;
+}
+console.log(bonusPercent);
+
+  // Check employee ID number length to determine if there will be an additional bonus.
+if (employee.employeeNumber.length <= 4) {
+  bonusPercent += 5
+}
+
+  // Check employee annual income and adjust to be within the min/max.
+
+  if (employee.annualSalary > 65000) {
+    bonusPercent -= 1
+  }
+
+  if (bonusPercent < 0) {
+    bonusPercent = 0
+  }
+
+  if (bonusPercent > 13) {
+    bonusPercent = 13
+  }
+    console.log(bonusPercent);
+
+    let totalBonus = 
   // return new object with bonus results
+  return {
+    name: employee.name,
+    bonusPercentage: bonusPercent,
+  }
 
 }
+calculateIndividualEmployeeBonus( {
+  name: 'Scout',
+  employeeNumber: '6243',
+  annualSalary: '74750',
+  reviewRating: 5
+});
+
+calculateIndividualEmployeeBonus({
+  name: 'Mayella',
+  employeeNumber: '89068',
+  annualSalary: '35000',
+  reviewRating: 1
+});
